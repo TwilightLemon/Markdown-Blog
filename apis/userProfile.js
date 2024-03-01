1686814f23a7c7463b3325cd78838ba4de72edb4
TwilightLemon <cz241126@live.com>
2024-03-01 11:24:23 +0800

    Email Verification

--- apis/userProfile.js
+++ apis/userProfile.js
@@ -1,7 +1,29 @@
-const User = require("../models/user");
+const User = require("../models/user")
+const emailService=require("nodemailer")
+
+async function sendVerificationCode(email){
+    let sender=emailService.createTransport({
+        host:'smtp.qiye.aliyun.com',
+        port:465,
+        secure:true,
+        auth:{
+            user:'blogadmin@twlm.space',
+            pass:'sS5Xc0zt7pCeaqL4'
+        }
+    })
+    let code=Math.floor(Math.random()*1000000)
+    let info=await sender.sendMail({
+        from:'BlogAdmin@twlm.space',
+        to:email,
+        subject:'Verification Code',
+        text:`Your verification code is ${code}`
+    })
+    return code
+}
+module.exports.sendVerificationCode=sendVerificationCode
 
 async function getName(email){
-    let user = await User.findOne({email: cookies.email});
-    return user?user.name:null;
+    let user = await User.findOne({email: cookies.email})
+    return user?user.name:null
 }
 module.exports.getName=getName
--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,7 @@ const auth = require('../apis/loginAuth')
 
 //Login Page
 router.get('/', (req, res) => {
-    res.render('account/login',{exist:false,confirmed:false,login:false})
+    res.render('account/login',{exist:false,confirmed:false,login:false,msg:""})
 })
 router.get('/signout', (req, res) => {
     res.cookie('loginToken', '', { maxAge: 1 });
@@ -21,29 +21,36 @@ router.post('/', async (req, res) => {
             auth.createLoginToken(res,user.email,user.saltedPsw,user.name);
             res.redirect('/')
         }else{
-            res.render('account/login', {login:false,exist: true, confirmed: true,email: req.body.email})
+            res.render('account/login', {login:false,exist: true, confirmed: true,email: req.body.email,msg:"Wrong Password!"})
         }
     }
     else {
         let user=await User.findOne({email:req.body.email})
         if (user) {
-            res.render('account/login', {login:false,exist: true, confirmed: true,email: req.body.email})
+            res.render('account/login', {login:false,exist: true, confirmed: true,email: req.body.email,msg:"welcome back, "+user.name+"!"})
         } else {
             //register
-            res.render('account/login', {login:false,exist: false, confirmed: true,email: req.body.email})
+            const up=require('../apis/userProfile')
+            let code=await up.sendVerificationCode(req.body.email)
+            res.cookie('VerificationCode',auth.signPsw(code),{maxAge:1000*60*5})
+            res.render('account/login', {login:false,exist: false, confirmed: true,email: req.body.email,msg:""})
         }
     }
 })
 router.post('/reg',async (req, res) => {
     console.log(req.body)
-    let user= new User({
-        email:req.body.email,
-        saltedPsw:req.body.password,
-        name:req.body.name
-    })
-    await user.save()
-    auth.createLoginToken(res,user.email,user.saltedPsw,user.name);
-    res.redirect('/')
+    if(auth.signPsw(req.body.code)===req.cookies.VerificationCode) {
+        let user = new User({
+            email: req.body.email,
+            saltedPsw: req.body.password,
+            name: req.body.name
+        })
+        await user.save()
+        auth.createLoginToken(res, user.email, user.saltedPsw, user.name);
+        res.redirect('/')
+    }else{
+        res.render('account/login', {login:false,exist: false, confirmed: true,email: req.body.email,msg:"Wrong Verification Code!"})
+    }
 })
 
 module.exports=router
